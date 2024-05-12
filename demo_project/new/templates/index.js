98b1ee5bdb19845b1cb14d12016ccc44cbb007e2
Ananya Chandrakar <134645478+ananyaaaaww@users.noreply.github.com>
2024-05-13 01:46:55 +0530

    Add files via upload

---
+++ demo_project/new/templates/index.js
@@ -0,0 +1,27 @@
+
+async function getTestPing() {
+    const response = await fetch("http://192.168.64.16:8000/run", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            "client": "local",
+            "tgt": "*", 
+            "fun": "test.ping",
+            "username": "ananya",
+            "password": "bing",
+            "eauth": "pam"
+        })
+    })
+    if (!response.ok) {
+        throw new Error(`API request failed: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data)
+
+    // const ddMenu = document.getElementById("dropDownMenu");
+    // ddMenu.innerHTML = ""; // Clear existing options
+
+}
